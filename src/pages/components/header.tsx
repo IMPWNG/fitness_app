d1802154fe1a6c80ca8ba86e5c23f38563dbe309
IMPWNG <matisse.ach+github@gmail.com>
2023-02-01 10:47:41 -0500

    packages + components

---
+++ src/pages/components/header.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Header() {
+  return (
+    <Head>
+      <title>Fitness App</title>
+      <meta name="description" content="NextJS Fitness App" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
