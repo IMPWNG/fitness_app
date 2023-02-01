045f538bb6768188560393c7f7bb8cb2969e6e5c
IMPWNG <matisse.ach+github@gmail.com>
2023-02-01 13:45:31 -0500

    fix link next13\

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -3,9 +3,24 @@ import headerNavLinks from "@/data/HeaderNavLinks";
 
 export default function Navbar() {
   return (
-    <div>
-    testdddd1
-    </div>
+    <nav className="fixed mt-8 h-full">
+      {headerNavLinks.map((link) => (
+        <div key={link.title} className="px-12 py-4">
+          <Link
+            href={link.href}
+            className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
+            onClick={() => {
+              console.log("clicked");
+            }}
+        
+          >
+            {link.title}
+          </Link>
+        </div>
+      ))}
+    </nav>
+
+  
   )
 }
 
