9630948c2262de4fd164d04cbbdbdf574979f49c
IMPWNG <matisse.ach+github@gmail.com>
2023-02-01 15:17:33 -0500

    todo : clear search

--- src/components/Exercices.tsx
+++ src/components/Exercices.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
-import { Box, Stack, Button, TextField, Typography } from "@mui/material";
-import { exercicesOptions, fetchData } from "@/utils/fetchData";
+import { Typography } from "@mui/material"
+import SearchExercices from "./SearchExercices"
 
 interface ExercicesProps {
 
@@ -8,38 +7,17 @@ interface ExercicesProps {
 
 function Exercices({ }: ExercicesProps) {
 
-  const [search, setSearch] = useState('');
 
-  const handleSearch = async(e: any) => {
-    if(search) {
-     const exercicesData = await fetchData(exercicesOptions);
-     console.log(exercicesData);
-    }
-  }
 
   return (
-    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+    <>
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
         Awesome Exercises You <br /> Should Know
       </Typography>
-      <Box position="relative" mb="72px">
-        <TextField
-          sx={{ width: { lg: '1170px', xs: '350px' } }}
-          value={search}
-          onChange={(e) => setSearch(e.target.value.toLowerCase())}
-          placeholder="Search Exercises"
-          type="text"
-        />
-        <Button className="search-btn" sx={{  color: '#fff', textTransform: 'none', height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
-          Search
-        </Button>
-      </Box>
-      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-       test
-      </Box>
-    </Stack>
-    
 
+      <SearchExercices />
+    </>
+    
   )
 }
 
--- src/components/SearchExercices.tsx
+++ src/components/SearchExercices.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Button, TextField, Typography } from "@mui/material";
+import { exercicesOptions, fetchData } from "@/utils/fetchData";
 
 function SearchExercices() {
+
+  const [search, setSearch] = useState('');
+  const [exercices, setExercices] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchExercicesData = async () => {
+  //     const bodyPartsData = await fetchData('');
+  // }, [])
+
+
+  const handleSearch = async (e: any) => {
+    if (search) {
+      const exercicesData = await fetchData(exercicesOptions);
+      console.log(exercicesData);
+
+      const searchedExercices = exercicesData.filter((exercice: any) => {
+        return exercice.name.toLowerCase().includes(search) || exercice.bodyPart.toLowerCase().includes(search) || exercice.equipment.toLowerCase().includes(search) || exercice.target.toLowerCase().includes(search)
+      }
+      )
+      setSearch('');
+      setExercices(searchedExercices);
+    }
+  }
+
   return (
-    <div>SearchExercices</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{ width: { lg: '1170px', xs: '350px' } }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button className="search-btn" sx={{ color: '#fff', textTransform: 'none', height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+          Search
+        </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+
+      </Box>
+    </Stack>
   )
 }
 
